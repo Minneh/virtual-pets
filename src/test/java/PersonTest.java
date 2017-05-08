b4457b12c5d1783a4aca6c1466f905b462856881
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-08 12:23:37 +0300

    complete tests and methods for Person, add Monster files

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,12 +1,68 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
 
 public class PersonTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void person_instantiatesCorrectly_true() {
     Person testPerson = new Person("Henry", "[email protected]");
     assertEquals(true, testPerson instanceof Person);
+  }
+
+  @Test
+  public void getName_personInstantiatesWithName_henry(){
+    Person testPerson = new Person("Henry", "[email protected]");
+    assertEquals("Henry", testPerson.getName());
+  }
+
+  @Test
+  public void getEmail_personInstantiatesWithEmail_String(){
+    Person testPerson = new Person("Henry", "[email protected]");
+    assertEquals("[email protected]", testPerson.getEmail());
+  }
+
+  @Test public void equals_returnsTrueIfNameAndEmailAreSame_true(){
+    Person testPerson = new Person("Henry", "[email protected]");
+    Person anotherPerson = new Person("Henry", "[email protected]");
+    assertTrue(testPerson.equals(anotherPerson));
+  }
+
+  @Test
+  public void save_insertsObjectIntoDatabase_Person(){
+    Person testPerson = new Person("Henry", "[email protected]");
+    testPerson.save();
+    assertTrue(Person.all().get(0).equals(testPerson));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    Person savedPerson = Person.all().get(0);
+    assertEquals(testPerson.getId(), savedPerson.getId());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfPerson_true(){
+    Person firstPerson = new Person("Henry", "henry@henry.com");
+    firstPerson.save();
+    Person secondPerson = new Person("Harriet", "harriet@harriet.com");
+    secondPerson.save();
+    assertEquals(true, Person.all().get(0).equals(firstPerson));
+    assertEquals(true, Person.all().get(1).equals(secondPerson));
+  }
+
+  @Test
+  public void find_returnsPersonWithSameId_secondPerson() {
+    Person firstPerson = new Person("Henry", "henry@henry.com");
+    firstPerson.save();
+    Person secondPerson = new Person("Harriet", "harriet@harriet.com");
+    secondPerson.save();
+    assertEquals(Person.find(secondPerson.getId()), secondPerson);
   }
 
 }
